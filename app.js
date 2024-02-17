bca691de53b9d140b2b21313dd90d7deaae8e7ac
Kaivalya Kulkarni <kaivalya@Kaivalyas-MacBook-Pro.local>
2024-02-17 15:57:01 +0530

    init

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express');
+// Imports
 const EmailServiceRouter = require('./Routes/EmailRoutes');
 const { MailUtil } = require('./EmailHandler/EmailHandler');
 const { mail_subject, get_case_token } = require('./Constants/Constants');
+
+const express = require('express');
 const app = express();
-app.get('',async (req,res)=>{
+
+app.use(express.static(__dirname + '/static'))
+
+app.get('/sendEmail',async (req,res)=>{
     const email_of_user = 'kaivalyakulkarni69@gmail.com'
     const mail = await new MailUtil();
     await mail.SendMailToUser(email_of_user,mail_subject,get_case_token('TEST_TOKEN'),res); 
     res.send('mail send')
 
+})
+
+app.get('',async (req,res)=>{
+    res.sendFile(__dirname + '/html/add_case.html')
 })
 
 // Mail Handler 
